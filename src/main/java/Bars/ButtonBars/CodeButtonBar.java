603136a2813b94588c5b5f1e22d8d1cf4739a3f4
ETOgaosion <gaoziyuan19@mails.ucas.ac.cn>
2023-12-13 03:36:01 +0800

    migrate to submodule

---
+++ src/main/java/Bars/ButtonBars/CodeButtonBar.java
@@ -0,0 +1,9 @@
+package Bars.ButtonBars;
+import Blocks.*;
+
+public class CodeButtonBar extends CommonTextButtonBar{
+    public CodeButtonBar(CommonTextBlock parentBlock){
+        super(parentBlock);
+        setTypeText("Code Block");
+    }
+}
